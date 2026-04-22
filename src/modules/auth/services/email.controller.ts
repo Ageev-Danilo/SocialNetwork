25a1c5709ee46f3ef7fccf05f09b8bfbaef83a31
Ageev-Danilo <danilaageev09@gmail.com>
2026-04-22 16:48:24 +0200

    my changes

--- src/modules/auth/services/email.controller.ts
+++ src/modules/auth/services/email.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { sendVerificationEmail } from './email.service';
+import { string } from 'yup';
 
 export const mailController = {
     async sendEmail(req: Request, res: Response) {
@@ -9,8 +10,10 @@ export const mailController = {
             if (!email) {
                 return res.status(400).json({ message: "Email обов'язковий" });
             }
+            const code =  Math.floor(100000 + Math.random() * 900000).toString();
 
-            await sendVerificationEmail(email);
+
+            await sendVerificationEmail(email, code);
             
             res.status(200).json({ message: "Лист відправлено!" });
         } catch (error: any) {
--- src/modules/auth/services/email.service.ts
+++ src/modules/auth/services/email.service.ts
@@ -5,14 +5,15 @@ const TEMPLATE_ID = 'template_nkzhzgh';
 const PUBLIC_KEY  = 'oo2vhpTMpp57OA9Tn';
 
 
+
 export async function sendVerificationEmail(
     toEmail: string,
     code: string,
-): Promise<void> {
+){
     await emailjs.send(
         SERVICE_ID,
         TEMPLATE_ID,
-        { to_email: toEmail, verification_code: code },
+        { to_email: toEmail, code: code },
         { publicKey: PUBLIC_KEY },
     );
 }
