0d0af745131ff40906143dbd74e0e58e8c6c9a63
Ageev-Danilo <danilaageev09@gmail.com>
2026-05-25 18:13:58 +0200

    ip

--- src/shared/api/base.ts
+++ src/shared/api/base.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { queryBaseHeaders } from './headers';
 
-const API_URL = process.env.EXPO_PUBLIC_API_URL ?? 'http://10.0.2.2:3000';
+const API_URL = process.env.EXPO_PUBLIC_API_URL ?? 'http://192.168.178.22:3000';
 
 export const baseApi = createApi({
     reducerPath: 'api',
